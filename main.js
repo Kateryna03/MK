6df8c5f8b6516e65967a571850c83936c2db15bb
Kateryna Gurienkova <gurienkova@gmail.com>
2021-06-07 18:57:56 +0300

    черновик

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-const Scorpion = {
+const scorpion = {
   name: "Scorpion",
-  hp: 20,
+  hp: 100,
   img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
   weapon: ["Longsword	", "Howling Sword", "Needle"],
   attack: function () {
     //console.log(Scorpion.name + "Fight...");
     console.log(this.name + "Fight...");
   },
+  player: 1,
+
+  changeHp: changeHp,
+  elHp: elHp,
+  renderHp: renderHp,
 };
 
-const Subzero = {
+const subzero = {
   name: "Subzero",
   hp: 100,
   img: "http://reactmarathon-api.herokuapp.com/assets/subzero.gif",
@@ -17,8 +22,16 @@ const Subzero = {
   attack: function () {
     console.log(this.name + "Fight...");
   },
+  player: 2,
+  changeHp: changeHp,
+  elHp: elHp,
+  renderHp: renderHp,
 };
 const elArenas = document.querySelector(".arenas");
+const elButton = document.querySelector(".button");
+
+//"body > div > div.arenas.arena1 > div.player2 > div.progressbar > div.life"
+
 //## Task #1
 
 // function createPlayer(player1, name, hp) {
@@ -54,7 +67,7 @@ const elArenas = document.querySelector(".arenas");
 //   elArenas.appendChild(elPlayer);
 // }
 // //## Task #2
-// // const elArenas = document.querySelectorAll(".arenas");
+// // const elArenas = document.querySelector(".arenas");
 // // console.log(elArenas);
 
 // createPlayer("player1", "SCORPION", 50);
@@ -64,34 +77,93 @@ const elArenas = document.querySelector(".arenas");
 // Передай в функцию createPlayer всего лишь два аргумента, 1 аргумент это строка 'player1' или 'player2' второй аргумент — это объект твоего игрока из задания Task#0.
 
 // Необходимые поля, такие как name, hp, img вставь в нужные места в коде.
-function createPlayer(player, obj) {
-  const elPlayer = document.createElement("div");
-  elPlayer.classList.add(player);
 
-  const elInnerOne = document.createElement("div");
-  elInnerOne.classList.add("progressbar");
+function createElement(tag, className) {
+  const ElTag = document.createElement(tag);
 
-  const elInnerTwo = document.createElement("div");
-  elInnerTwo.classList.add("character");
+  if (className) {
+    ElTag.classList.add(className);
+  }
+  return ElTag;
+}
+
+function createPlayer(obj) {
+  const elPlayer = createElement("div", "player" + obj.player);
+  const elInnerOne = createElement("div", "progressbar");
+  const elInnerTwo = createElement("div", "character");
+  const divInProgressbar1 = createElement("div", "life");
+  const divInProgressbar2 = createElement("div", "name");
+  const elImg = createElement("img");
+
+  divInProgressbar1.style.width = `${obj.hp}%`;
+  divInProgressbar2.innerText = obj.name;
+  elImg.src = obj.img;
 
   elPlayer.appendChild(elInnerOne);
   elPlayer.appendChild(elInnerTwo);
-
-  const divInProgressbar1 = document.createElement("div");
-  divInProgressbar1.classList.add("life");
-  divInProgressbar1.style.width = `${obj.hp}%`;
-
-  const divInProgressbar2 = document.createElement("div");
-  divInProgressbar2.classList.add("name");
-  divInProgressbar2.innerText = obj.name;
-
   elInnerOne.appendChild(divInProgressbar1);
   elInnerOne.appendChild(divInProgressbar2);
-
-  const elImg = document.createElement("img");
-  elImg.src = obj.img;
   elInnerTwo.appendChild(elImg);
-  elArenas.appendChild(elPlayer);
+
+  return elPlayer;
 }
-createPlayer("player1", Scorpion);
-createPlayer("player2", Subzero);
+elArenas.appendChild(createPlayer(scorpion));
+elArenas.appendChild(createPlayer(subzero));
+
+function getRandom(n) {
+  return Math.ceil(Math.random() * n);
+}
+
+function changeHp(n) {
+  this.hp -= n;
+  if (this.hp <= 0) {
+    this.hp = 0;
+  }
+  return this.hp;
+}
+function elHp() {
+  return (element = document.querySelector(".player" + this.player));
+}
+
+function renderHp() {
+  const elPlayerLife = document.querySelector(".player" + pl.player + " .life");
+  return (elPlayerLife.style.width = player.hp + "%");
+}
+// Функция changeHP должна в аргументах принимать, на какое кол-во надо изменять HP. И решать, нужно ли отнимать или ставить 0. Больше ничего эта функция не должна делать.
+// function changeHp(n) {
+//   let newHp;
+//   if (this.hp > 0) {
+//     newHp = player.hp - n;
+//   }
+// }
+
+function playerWin(name) {
+  const elLoseTitle = createElement("div", "loseTitle");
+  if (name) {
+    elLoseTitle.innerText = name + " win";
+  } else {
+    elLoseTitle.innerText = "draw";
+  }
+
+  return elLoseTitle;
+}
+
+elButton.addEventListener("click", function () {
+  scorpion.changeHp(getRandom(20));
+  subzero.changeHp(getRandom(20));
+  // changeHp(scorpion);
+  // changeHp(subzero);
+
+  if (scorpion.hp === 0 || subzero.hp === 0) {
+    elButton.disabled = true;
+  }
+
+  if (scorpion.hp === 0 && scorpion.hp < subzero.hp) {
+    elArenas.appendChild(playerWin(subzero.name));
+    console.log(elArenas);
+  } else if (subzero.hp === 0 && subzero.hp < scorpion.hp) {
+    elArenas.appendChild(playerWin(scorpion.name));
+  } else if (subzero.hp === 0 && scorpion.hp === 0) {
+    elArenas.appendChild(playerWin());
+  }
+});
